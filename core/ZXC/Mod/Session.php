923e7554952bd47e75ed27c7cad931add33cb19e
nikolaygiman <gnl@MAIL.RU>
2017-12-05 00:06:46 +0300

    test-0006

--- core/ZXC/Mod/Session.php
+++ core/ZXC/Mod/Session.php
@@ -9,7 +9,59 @@
 namespace ZXC\Mod;
 
 
-class Session
-{
+use ZXC\Interfaces\Module;
 
+class Session implements Module
+{
+    protected $time;
+    private $name;
+    private $main;
+    private $current;
+
+    public function __construct(array $config = [])
+    {
+        $this->start();
+        $_SESSION['id'] = session_id();
+        $_SESSION['start'] = $this->getTime();
+    }
+
+    public function initialize()
+    {
+        // TODO: Implement initialize() method.
+    }
+
+    public function start()
+    {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+    }
+
+    public function end()
+    {
+        $_SESSION = array();
+    }
+
+    public function isEnabled()
+    {
+
+    }
+
+    public function getTime()
+    {
+
+        $hour = date('H');
+        $min = date('i');
+        $sec = date('s');
+        $month = date('m');
+        $day = date('d');
+        $year = date('y');
+
+        return mktime($hour, $min, $sec, $month, $day, $year);
+    }
+
+    public function __destruct()
+    {
+        unset($this);
+    }
 }
---
+++ core/ZXC/Traits/GetSet.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikolaygiman
+ * Date: 04/12/2017
+ * Time: 23:59
+ */
+
+namespace ZXC\Traits;
+
+
+trait GetSet
+{
+    private $ignoreList = [];
+
+    public function get($key)
+    {
+        if (isset($this->$key) && !isset($this->ignoreList[$key])) {
+            return $this->$key;
+        } else {
+            return false;
+        }
+    }
+
+    public function set($key, $value)
+    {
+        if (!isset($this->ignoreList[$key])) {
+            return $this->$key = $value;
+        } else {
+            return false;
+        }
+    }
+}
