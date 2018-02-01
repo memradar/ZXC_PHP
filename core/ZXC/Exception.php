83864f83e02a05aa8e1b1e5fb0691b2e2bf2490f
nikolaygiman <gnl@MAIL.RU>
2018-02-01 21:44:31 +0300

    test-xxxxx28

---
+++ core/ZXC/Exception.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikolaygiman
+ * Date: 01/02/2018
+ * Time: 21:35
+ */
+
+namespace ZXC;
+
+
+class Exception extends \Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
