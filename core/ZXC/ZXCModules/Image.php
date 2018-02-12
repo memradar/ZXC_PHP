efa1d60ab66d4d2af3fc697839f187472835356d
nikolaygiman <gnl@MAIL.RU>
2018-02-13 00:17:42 +0300

    test-xxxxx39

---
+++ core/ZXC/ZXCModules/Image.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikolaygiman
+ * Date: 13/02/2018
+ * Time: 00:08
+ */
+
+namespace ZXC\ZXCModules;
+
+
+class Image
+{
+    public function crop()
+    {
+
+    }
+
+    public function load()
+    {
+
+    }
+
+    public function save(string $path)
+    {
+
+    }
+
+    public function captcha()
+    {
+
+    }
+}
