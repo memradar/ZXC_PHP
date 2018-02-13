be0acc1261687831ab753eb5a7671f5992f64c81
nikolaygiman <gnl@MAIL.RU>
2018-02-14 00:15:35 +0300

    test-xxxxx40

--- core/ZXC/Classes/FS.php
+++ core/ZXC/Classes/FS.php
@@ -1,15 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nikolaygiman
- * Date: 03/01/2018
- * Time: 16:54
- */
 
-namespace ZXC\Mod;
+namespace ZXC\Classes;
 
+use ZXC\Patterns\Singleton;
 
 class FS
 {
+    use Singleton;
 
+    public static function read(string $filePath)
+    {
+        $result = @file_get_contents($filePath);
+        if ($result) {
+            return $result;
+        }
+        return false;
+    }
+
+    public static function write(string $filePath, $data, $appendData)
+    {
+        return file_put_contents($filePath, $data, $appendData ? FILE_APPEND : LOCK_EX);
+    }
 }
--- core/ZXC/ZXCModules/Image.php
+++ core/ZXC/ZXCModules/Image.php
@@ -1,24 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nikolaygiman
- * Date: 13/02/2018
- * Time: 00:08
- */
 
 namespace ZXC\ZXCModules;
 
 
+use ZXC\Classes\FS;
+
 class Image
 {
-    public function crop()
+    protected $imageResource;
+    protected $fileName;
+
+    /**
+     * Image constructor.
+     * @param string $filePath
+     * @throws \Exception
+     */
+    public function __construct(string $filePath)
+    {
+        if ($filePath) {
+            $this->imageResource = $this->load($filePath);
+            if (!$this->imageResource) {
+                throw new \Exception('Can not read file ' . $filePath);
+            }
+        }
+    }
+
+    public function crop($x1, $y1, $x2, $y2)
     {
 
     }
 
-    public function load()
+    public function load(string $path)
     {
-
+        if (file_exists($path)) {
+            $string = FS::read($path);
+            if ($string) {
+                return imagecreatefromstring($string);
+            }
+        }
+        return false;
     }
 
     public function save(string $path)
@@ -26,8 +46,31 @@ class Image
 
     }
 
-    public function captcha()
+    public function captcha($width = 160, $height = 50, $text = '', $font = false)
     {
+//        imagecopyresampled();
+        $img = imagecreatetruecolor($width, $height);
+        $grey = imagecolorallocate($img, 128, 128, 128);
+
+        imagettftext($img, 15, 0, 11, 21, $grey, ZXC_ROOT . '/../../log/times_new_yorker.ttf', '$text');
+
+        $red   = imagecolorallocate($img, 255,   0,   0);
+        imagearc($img, 0, 0, 30, 15, 0, 360, $red);
+        imagearc($img, 20, 10, 30, 15, 0, 360, $red);
+
+
+        $white = imagecolorallocate($img, 0xFF, 0xFF, 0xFF);
+
+        imagedashedline($img, 50, 25, 50, 75, $white);
+
+
+        imagepng($img, ZXC_ROOT . '/../../log/test.png', 9);
+        imagedestroy($img);
+    }
+
+    public function create($width, $height)
+    {
+        return imagecreatetruecolor($width, $height);
 
     }
 }
